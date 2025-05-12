fe3201ce01cb0ca4e33816c5c8863858355c8aba
vimanhtuyen <vimanhtuyen@gmail.com>
2025-05-12 10:38:44 +0700

    fix: add STATIC_ROOT and enable whitenoise

--- travel_blog/settings.py
+++ travel_blog/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -68,3 +69,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
